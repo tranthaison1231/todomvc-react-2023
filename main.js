4f284d65c3ebe9e9116f8e62e2f25ab4d3fe3310
son.tran <son.tran@team.enouvo.com>
2023-03-04 13:51:05 +0700

    feat(master): add edit todo feature

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-function insertHTML(el, html) {
-  el.insertAdjacentHTML('afterbegin', html);
-}
+const getURLHash = () => document.location.hash.replace(/^#\//, '');
 
 function delegate(el, selector, event, handler) {
   el.addEventListener(event, (e) => {
@@ -8,18 +6,20 @@ function delegate(el, selector, event, handler) {
   });
 }
 
+const filterNotCompletedTodos = (todos) => todos.filter((todo) => !todo.completed);
+
 function createTodoItemEl({ value, id, completed }) {
   const li = document.createElement('li');
   li.dataset.id = id;
-  li.className = 'py-[16px] px-[20px] border-solid border-b-2 border-gray-300 flex items-center justify-between';
-  insertHTML(
-    li,
+  li.className = 'py-[16px] group px-[20px] border-solid border-b-2 border-gray-300 flex items-center justify-between';
+  li.insertAdjacentHTML(
+    'afterbegin',
     ` 
-      <div class="flex items-center">
+      <div class="flex items-center w-full">
         <i data-todo="toggle" class='bx ${completed ? 'bx-check-square' : 'bx-square'} text-[30px] cursor-pointer'></i>
-        <span data-todo="value" class="pl-[10px] ${completed ? 'line-through' : ''}"></span>
+        <div contenteditable="true" data-todo="value" class="pl-[10px] w-full ${completed ? 'line-through' : ''}"></div>
       </div>
-      <i data-todo="remove" class='bx bx-trash text-[30px] cursor-pointer'></i>
+      <i data-todo="remove" class='bx bx-trash text-[30px] cursor-pointer invisible group-hover:visible'></i>
     `
   );
   li.querySelector('[data-todo="value"]').textContent = value;
@@ -32,6 +32,7 @@ function App() {
   const inputEl = document.getElementById('input');
   const titleEl = document.getElementById('title');
   const listEl = document.getElementById('list');
+  const countEl = document.getElementById('count');
   const eventTarget = new EventTarget();
 
   function saveTodos() {
@@ -39,7 +40,19 @@ function App() {
   }
 
   function renderTodos() {
-    listEl.replaceChildren(...todos.map((todo) => createTodoItemEl(todo)));
+    const filter = getURLHash();
+    let filterTodos = [...todos];
+    if (filter === 'active') filterTodos = todos.filter((todo) => !todo.completed);
+    else if (filter === 'completed') filterTodos = todos.filter((todo) => todo.completed);
+    countEl.innerHTML = `${filterNotCompletedTodos(todos).length} items left`;
+    listEl.replaceChildren(...filterTodos.map((todo) => createTodoItemEl(todo)));
+    document.querySelectorAll(`[data-todo="filters"] a`).forEach((el) => {
+      if (el.matches(`[href="#/${filter}"]`)) {
+        el.classList.add('checked');
+      } else {
+        el.classList.remove('checked');
+      }
+    });
   }
 
   function readTodosInStorage() {
@@ -47,7 +60,7 @@ function App() {
   }
 
   inputEl.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter' || event.keyCode === 13) {
+    if ((event.key === 'Enter' || event.keyCode === 13) && inputEl.value.trim() !== '') {
       const newTodo = { value: inputEl.value, id: crypto.randomUUID(), completed: false };
       todos.push(newTodo);
       const todoEl = createTodoItemEl(newTodo);
@@ -57,8 +70,7 @@ function App() {
 
   delegate(listEl, '[data-todo="toggle"]', 'click', (e) => {
     const el = e.target.closest('[data-id]');
-    const newTodo = todos.find((todo) => todo.id === el.dataset.id);
-    todos = todos.map((todo) => (todo.id === newTodo.id ? { ...todo, completed: !todo.completed } : todo));
+    todos = todos.map((todo) => (todo.id === el.dataset.id ? { ...todo, completed: !todo.completed } : todo));
     eventTarget.dispatchEvent(new CustomEvent('save'));
   });
 
@@ -68,19 +80,25 @@ function App() {
     eventTarget.dispatchEvent(new CustomEvent('save'));
   });
 
+  delegate(listEl, '[data-todo="value"]', 'keydown', (e) => {
+    const el = e.target.closest('[data-id]');
+    if (event.keyCode === 13) {
+      e.preventDefault();
+      const content = el.querySelector('[data-todo="value"]').textContent;
+      todos = todos.map((todo) => (todo.id === el.dataset.id ? { ...todo, value: content } : todo));
+      eventTarget.dispatchEvent(new CustomEvent('save'));
+    }
+  });
+
   eventTarget.addEventListener('save', () => {
     saveTodos();
     renderTodos();
   });
 
-  window.addEventListener(
-    'storage',
-    () => {
-      readTodosInStorage();
-      saveTodos();
-    },
-    false
-  );
+  window.addEventListener('hashchange', () => {
+    renderTodos();
+  });
+
   readTodosInStorage();
   renderTodos();
 }
