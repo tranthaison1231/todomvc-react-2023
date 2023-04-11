0096fd0608f60bdda62b5c2609752e1426ff011d
son.tran <son.tran@team.enouvo.com>
2023-04-11 14:23:15 +0700

    feat(master): add Todo, Todos, Loading components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './style.css'
 
 createRoot(document.getElementById('app') as HTMLElement).render(
-  <App />
+  <div className="bg-[#f5f5f5] py-0 px-[10%]">
+    <App />
+  </div>
 )
